f981cbf9beff84027a1bbb75b4fe58e81d4a3dc3
burtonzr <burtonzr@gmail.com>
2020-04-30 08:29:16 -0500

    Admin and regular user features for techstore

--- src/components/pages/TechStore.js
+++ src/components/pages/TechStore.js
@@ -2,6 +2,8 @@ import React, { Component } from 'react';
 
 var loadjs = require('loadjs');
 
+const editproduct = "../images/editproduct.png";
+
 class TechStore extends Component {
 
     componentDidMount() {
@@ -20,9 +22,86 @@ class TechStore extends Component {
                         The application was developed using ColdFusion, JavaScript (jQuery), and SQL Server.
                     </p>
                 </div>
+                <div className="gridTitle" style={color1}>
+                    Adminstrator Features
+                    <hr className="sep" />
+                    <p>
+                        The staff members that get admistrative access are the Technology Purchaser, 
+                        Data Systems staff, and building adminstrators. 
+                    </p>
+                </div>
+                <div className="features" style={color1}>
+                    <div class="row">
+                        <div class="shadow-none rounded col-12 col-sm-6 col-md-4 pt-3">
+                            <h5 style={centerFeatureTitle}>Edit Product</h5>
+                            <p className="pt-2">
+                                This feature allows the Technology Purchaser to click the edit product
+                                button of the product that they want to edit and then they can update the 
+                                name, description, model number, status, image, etc.. 
+                            </p>
+                            <img src={editproduct} className="img-fluid" />
+                        </div>
+                        <div class="shadow-none rounded col-12 col-sm-6 col-md-4 pt-3">
+                            <h5 style={centerFeatureTitle}>Add Product</h5>
+                            <p className="pt-2">
+
+                            </p>
+                        </div>
+                        <div class="shadow-none rounded col-12 col-sm-6 col-md-4 pt-3">
+                            <h5 style={centerFeatureTitle}>View All Orders</h5>
+                            <p className="pt-2">
+
+                            </p>
+                        </div>
+                    </div>
+                </div>
+                <div className="gridTitle" style={color1}>
+                    Average User Features
+                    <hr className="sep" />
+                    <p>
+                        Regular users include most staff members that might need technology parts or equipment, 
+                        like a new laptop or power adapter. 
+                    </p>
+                </div>
+                <div className="features" style={color1}>
+                    <div class="row">
+                        <div class="shadow-none rounded col-12 col-sm-6 col-md-4">
+                            <h5 style={centerFeatureTitle}>Add to Cart</h5>
+                            <p className="pt-2">
+                                This feature allows the Technology Purchaser to click the edit product
+                                button of the product that they want to edit and then they can update the 
+                                name, description, model number, status, image, etc.. 
+                            </p>
+                        </div>
+                        <div class="shadow-none rounded col-12 col-sm-6 col-md-4">
+                            <h5 style={centerFeatureTitle}>Create Multiple Carts</h5>
+                            <p className="pt-2">
+                                This feature allows the Technology Purchaser to click the edit product
+                                button of the product that they want to edit and then they can update the 
+                                name, description, model number, status, image, etc.. 
+                            </p>
+                        </div>
+                        <div class="shadow-none rounded col-12 col-sm-6 col-md-4">
+                            <h5 style={centerFeatureTitle}>Add Custom Product to Cart</h5>
+                            <p className="pt-2">
+                                This feature allows the Technology Purchaser to click the edit product
+                                button of the product that they want to edit and then they can update the 
+                                name, description, model number, status, image, etc.. 
+                            </p>
+                        </div>
+                    </div>
+                </div>
             </div>
         );
     }
+}
+
+const centerFeatureTitle = {
+    textAlign: 'center'
+}
+
+const color1 = {
+    backgroundColor: '#ecf2f9'
 }
 
 export default TechStore;
